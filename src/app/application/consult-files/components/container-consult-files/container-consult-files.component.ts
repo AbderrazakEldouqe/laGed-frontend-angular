1a0a4853a56c8eddd62c223147f99d0dbb2c40a6
Abderrazak Eldouqe <abderrazak.eldouqe1@gmail.com>
2021-08-25 11:17:02 +0100

    Refactor Code ConsultFiles Module & Add Comments

--- src/app/application/consult-files/components/container-consult-files/container-consult-files.component.ts
+++ src/app/application/consult-files/components/container-consult-files/container-consult-files.component.ts
@@ -8,14 +8,26 @@ import { ConsultFilesService } from '../../services/consult-files.service';
   styleUrls: ['./container-consult-files.component.css'],
 })
 export class ContainerConsultFilesComponent implements OnInit, OnDestroy {
+  /* Start Variables */
   private subs = new SubSink();
   filesData: any[] = [];
+
+  /* End Variables */
   constructor(private consultFilesService: ConsultFilesService) {}
 
+  /**
+   * ngOnInit
+   * * is a life cycle hook called by Angular to indicate that Angular is done creating the component
+   */
   ngOnInit(): void {
     this.getAll();
   }
 
+  /* Start Services */
+  /**
+   * getAll
+   * * It is called for get data From Backend
+   */
   getAll(): void {
     this.subs.add(
       this.consultFilesService.getAll().subscribe((res: any[]) => {
@@ -24,6 +36,11 @@ export class ContainerConsultFilesComponent implements OnInit, OnDestroy {
     );
   }
 
+  /* End Services */
+  /**
+   * ngOnDestroy
+   * * It is called for cleanup logic when a component is destroyed
+   */
   ngOnDestroy(): void {
     this.subs.unsubscribe();
   }
--- src/app/application/consult-files/components/list-consult-files/list-consult-files.component.ts
+++ src/app/application/consult-files/components/list-consult-files/list-consult-files.component.ts
@@ -1,11 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-list-consult-files',
   templateUrl: './list-consult-files.component.html',
   styleUrls: ['./list-consult-files.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ListConsultFilesComponent implements OnInit {
+  /* Start Variables */
   @Input() filesData: any[] = [];
 
   config = {
@@ -17,10 +24,15 @@ export class ListConsultFilesComponent implements OnInit {
   tableSizes = [4, 8, 10, 14];
   filter = '';
 
+  /* End Variables */
   constructor() {}
 
   ngOnInit(): void {}
 
+  /**
+   * onTableSizeChange
+   * * For change size Pagination per page
+   */
   onTableSizeChange(event: any): void {
     this.config.itemsPerPage = event.target.value;
     this.config.currentPage = 1;
